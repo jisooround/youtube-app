107af4f82684f11097d9acb73dc808d8c7873740
jisooround <9utesleben@gmail.com>
2023-01-16 17:21:34 +0900

    Feat : Card Component.

---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = () => {
+  return (
+    <Container>
+      <Image><img src="" alt="" /></Image>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 359px;
+  height: 280px;
+  background-color: pink;
+`
+
+const Image = styled.div`
+  width: 100%;
+  height: 202px;
+  background-color: black;
+`
+
+export default Card
