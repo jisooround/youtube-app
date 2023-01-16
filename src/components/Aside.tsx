742aa9d25448c808371ed092be2341ee809d179b
jisooround <9utesleben@gmail.com>
2023-01-16 17:22:40 +0900

    Docs : Aside Components, Home pages

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,4 +1,32 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
 const Aside = () => {
-  return <aside>Aside</aside>;
+  const [fold, setFold] = useState(false);
+  console.log(fold)
+
+  if(fold === true) {
+    return (
+      <Fold>Aside</Fold>
+      );
+  } else {
+    return (
+    <Unfold>Aside</Unfold>
+  )}
 };
+
+const Fold = styled.div`
+  width: 72px;
+  height: 100vh;
+  position: absolute;
+  background-color: green;
+`
+
+const Unfold = styled.div`
+  width: 240px;
+  height: 100vh;
+  position: absolute;
+  background-color: green;
+`
+
 export default Aside;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,39 @@
+import React, { useState } from "react"
+import styled from "styled-components";
+import Card from "../../components/Card";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [fold, setFold] = useState(false);
+
+  if(fold === true) {
+    return (
+      <Fold>
+        <Card />
+      </Fold>
+      );
+  } else {
+    return (
+      <Unfold>
+        <Card />
+      </Unfold>
+      );
+  }
 };
+
+const Fold = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: beige;
+  margin-left: 72px;
+`
+
+const Unfold = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: beige;
+  margin-left: 240px
+`
+
 export default Home;
