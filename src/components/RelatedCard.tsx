8a5a7cdc6496c662a6061ad42297eb9de6e4370b
hoheesu <hoheesu@naver.com>
2023-01-17 16:38:53 +0900

    feat: 상세 데이터 받아오기

--- src/components/RelatedCard.tsx
+++ src/components/RelatedCard.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { instance } from "../api/api";
+import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 
 function RelatedCard({ item }: any) {
   const navigate = useNavigate();
-  console.log(item);
   const date: any = new Date(item.snippet.publishTime);
   let now: any = new Date(); // 현재 날짜 및 시간
-
+  const [videoView, setVideoView] = useState<any>('')
+  const [videoTime, setVideoTime] = useState<any>('')
   function displayedAt(createdAt: any) {
     const milliSeconds = now - createdAt;
     const seconds = milliSeconds / 1000;
@@ -17,7 +18,7 @@ function RelatedCard({ item }: any) {
     const hours = minutes / 60;
     if (hours < 24) return `${Math.floor(hours)}시간 전`;
     const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
+    if (days < 7) return `${Math.floor(days)}일 전`; 
     const weeks = days / 7;
     if (weeks < 5) return `${Math.floor(weeks)}주 전`;
     const months = days / 30;
@@ -25,23 +26,72 @@ function RelatedCard({ item }: any) {
     const years = days / 365;
     return `${Math.floor(years)}년 전`;
   }
-  console.log();
-  const goToVideo = async (item: any) => {
+
+  function nFormatter(num:number) {
+    if (num >= 1000000000) {
+       return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+    }
+    if (num >= 1000000) {
+       return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    }
+    if (num >= 1000) {
+       return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    }
+    return num;
+  }
+
+  const time = (input:string)=>{
+    let reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+    let hours = '', minutes = '', seconds = ''
+    if (reptms.test(input)) {
+      let matches:any = reptms.exec(input);
+      if (matches[1]) hours = matches[1];
+      if (matches[2]) minutes = matches[2];
+      if (matches[3]) seconds = matches[3];
+    }
+    if (minutes.length === 1) minutes = `0${minutes}`
+    if (seconds.length === 1) seconds = `0${seconds}`
+    if (hours.length === 1) {
+    hours = `${hours}`
+    return `${hours}:${minutes}:${seconds}`
+    }
+    if(hours.length === 0)
+      return `${minutes}:${seconds}`
+    }
+
+
+  const getDetail = async (videoId=item.id.videoId) => {
     const res = await instance.get(
-      `/videos?part=snippet&part=contentDetails&part=player&part=statistics&id=${item}`,
+      `/videos?part=snippet&part=contentDetails&part=player&part=statistics&id=${videoId}`,
     );
-    console.log(res);
-    navigate(`/watch/${item}`);
+    const view = nFormatter(res.data.items[0].statistics.viewCount)
+    setVideoView(view)
+    const vTime =time(res.data.items[0].contentDetails.duration)
+    setVideoTime(vTime)
+  }
+
+  const goToVideo = (videoId: any) => {
+    navigate(`/watch/${videoId}`);
   };
+
+  useEffect(()=>{
+    getDetail()
+  },[])
 
   return (
     <div onClick={() => goToVideo(item.id.videoId)}>
-      <img src={item.snippet.thumbnails.default.url} />
+      <div className="preview">
+        <img src={item.snippet.thumbnails.default.url} />
+      </div>
       <p>{item.snippet.title}</p>
       <p>{item.snippet.channelTitle}</p>
       <p>{displayedAt(date)}</p>
+      <p>{videoView}</p>
+      <p>{videoTime}</p>
     </div>
   );
 }
-
+// let Img = styled.img`
+//   padding
+// `
 export default RelatedCard;
--- src/components/RelatedVideo.tsx
+++ src/components/RelatedVideo.tsx
@@ -14,7 +14,6 @@ function RelatedVideo(): React.ReactElement {
 
   useEffect(() => {
     getVideoData();
-    console.log(data);
   }, []);
 
   return (
@@ -25,5 +24,73 @@ function RelatedVideo(): React.ReactElement {
     </div>
   );
 }
+
+export interface Root {
+  kind: string
+  etag: string
+  items: Item[]
+}
+
+export interface Item {
+  kind: string
+  etag: string
+  id: Id
+  snippet: Snippet
+}
+
+export interface Id {
+  kind: string
+  videoId: string
+}
+
+export interface Snippet {
+  publishedAt: string
+  channelId: string
+  title: string
+  description: string
+  thumbnails: Thumbnails
+  channelTitle: string
+  liveBroadcastContent: string
+  publishTime: string
+}
+
+export interface Thumbnails {
+  default: Default
+  medium: Medium
+  high: High
+  standard: Standard
+  maxres: Maxres
+}
+
+export interface Default {
+  url: string
+  width: number
+  height: number
+}
+
+export interface Medium {
+  url: string
+  width: number
+  height: number
+}
+
+export interface High {
+  url: string
+  width: number
+  height: number
+}
+
+export interface Standard {
+  url: string
+  width: number
+  height: number
+}
+
+export interface Maxres {
+  url: string
+  width: number
+  height: number
+}
+
 
 export default RelatedVideo;
