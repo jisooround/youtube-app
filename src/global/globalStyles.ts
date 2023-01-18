29584415beed2da6ce0252f930a9527c109aeddf
yujiseok <koesijuy@gmail.com>
2023-01-18 14:30:29 +0900

    feat: add prefers-color-scheme

--- src/global/globalStyles.ts
+++ src/global/globalStyles.ts
@@ -6,7 +6,22 @@ ${reset}
 
   body {
     font-family: 'Roboto', sans-serif;
+    background-color: #fff;
+    color: #0f0f0f
   }
+
+
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #0F0F0F;
+    color: #fff
+  }
+
+  svg {
+    color: #fff
+  }
+
+}
 
 `;
 
