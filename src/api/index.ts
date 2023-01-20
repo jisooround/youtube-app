c925a3bed9d08e937bae194faadd4b8dd83cf3be
Jaeheon-So <sjh-98@naver.com>
2023-01-20 15:08:53 +0900

    test commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3",
   params: {
-
-    key: import.meta.env.VITE_API_KEY_WOO,
-
+    key: import.meta.env.VITE_API_KEY_YU,
   },
 });
- 
